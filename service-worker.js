c932869dbb1d5fd9b897aa36eb2433d0f4ad049d
Sayantan <sp@unthread.world>
2024-01-28 20:12:18 +0530

    randomness redefined

--- service-worker.js
+++ service-worker.js
@@ -7,7 +7,7 @@ const FILES_TO_CACHE = [
   "/junkyard-game/logo.svg",
   "/junkyard-game/manifest.json",
   "/junkyard-game/i-am-a-deceptive-cheater.html",
-  "/junkyard-game/assets/index-28682539.js",
+  "/junkyard-game/assets/index-02430b62.js",
   "/junkyard-game/assets/index-445f2335.css",
   "/junkyard-game/assets/stats/0.png",
   "/junkyard-game/assets/stats/1.png",
